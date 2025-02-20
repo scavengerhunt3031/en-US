cc59822de58ec7ef513f05c8333b1daec8024ff8
AdwaithVS <adwaithvs512@gmail.com>
2025-02-20 20:06:02 +0530

    page not found

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function triggerClue(isPressed) {
   var clue = document.getElementById("clue");
   if (isPressed) {
-    clue.style.display = "block";  // Show the clue when the mouse button is pressed
+    clue.style.display = "block";
   } else {
-    clue.style.display = "none";   // Hide the clue when the mouse button is released
+    clue.style.display = "none";
   }
 }
